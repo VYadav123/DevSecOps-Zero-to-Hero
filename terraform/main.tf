072d522b166f0ae707dfd46d6e64b2b0f33157c5
VYadav123 <77671088+VYadav123@users.noreply.github.com>
2026-01-29 11:34:54 +0530

    Refactor Terraform configuration for Azure resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,36 +1,20 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.6"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
-  # env-based auth: ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
 }
 
-resource "random_string" "suffix" {
-  length  = 6
-  upper   = false
-  special = false
+resource "azurerm_resource_group" "example" {
+  name     = "vault-demo-rg"
+  location = "East US"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "vault-demo-rg-${random_string.suffix.result}"
-  location = "West Europe"
-}
-
-resource "azurerm_storage_account" "sa" {
-  name                     = "vdemostg${random_string.suffix.result}"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+resource "azurerm_storage_account" "example" {
+  name                     = "vaultstorage${random_id.suffix.hex}"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "random_id" "suffix" {
+  byte_length = 4
 }
